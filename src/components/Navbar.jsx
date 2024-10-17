fbab1a1d6eda3a9eeb9944d0733cc93ffc9b1722
sohrab09 <sohrab.cse9@gmail.com>
2024-10-17 19:51:03 +0600

    Wishlist Page design and Development completed.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,6 @@ function NavBar() {
 
     const { wishlist } = useContext(WishlistContext);
 
-    console.log("wishlist", wishlist);
-
     return (
         <>
             <nav className="navbar">
